6cd6532e52093335ebc17001d62153ddd159b39f
Brandom-Baruch <baruchoo94@gmail.com>
2021-04-15 15:27:36 -0500

    editando tabla

--- database/migrations/2020_10_12_203933_create_grupos_table.php
+++ database/migrations/2020_10_12_203933_create_grupos_table.php
@@ -14,7 +14,7 @@ class CreateGruposTable extends Migration
     public function up()
     {
         Schema::create('grupos', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('grado');
             $table->char('grupo');
